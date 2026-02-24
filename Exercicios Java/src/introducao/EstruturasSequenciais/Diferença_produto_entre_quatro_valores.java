5b71ff23eb81d99bd338a59bea7a847b15b3f5ea
JoaoLucas <joaolucasdev28@gmail.com>
2026-02-24 14:53:53 -0300

    Add exercise: difference between products of four numbers

--- Exercicios Java/src/introducao/EstruturasSequenciais/Diferença_produto_entre_quatro_valores.java
+++ Exercicios Java/src/introducao/EstruturasSequenciais/Diferença_produto_entre_quatro_valores.java
@@ -1,4 +1,29 @@
 package introducao.EstruturasSequenciais;
+import java.util.Scanner;
 
 public class Diferença_produto_entre_quatro_valores {
+    static void main() {
+
+        int A, B, C, D;
+        int diferenca;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o valor de A: ");
+            A = input.nextInt();
+        System.out.println("Digite o valor de B: ");
+            B = input.nextInt();
+        System.out.println("Digite o valor de C: ");
+            C = input.nextInt();
+        System.out.println("Digite o valor de D: ");
+            D = input.nextInt();
+
+        if ((A*B) > (C*D) ) {
+
+            diferenca = (A*B)-(C*D);
+        }else{
+            diferenca = (C*D)-(A*B);
+        }
+        System.out.println("A diferença do produto dos valores é: " + diferenca);
+    }
 }
