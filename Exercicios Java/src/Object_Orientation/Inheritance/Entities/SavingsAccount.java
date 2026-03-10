e7435ffb5d3c53ea0ffe255e1271f3000e4ba341
JoaoLucas <joaolucasdev28@gmail.com>
2026-03-10 15:50:33 -0300

    Upd: Done tests with Inheritance classes

---
+++ Exercicios Java/src/Object_Orientation/Inheritance/Entities/SavingsAccount.java
@@ -0,0 +1,26 @@
+package Object_Orientation.Inheritance.Entities;
+
+public class SavingsAccount extends Account {
+
+    private double InterestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, Double balance, String holder, double interestRate) {
+        super(number, balance, holder);
+        this.InterestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return InterestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        InterestRate = interestRate;
+    }
+    public void updateBalance(){
+        balance += balance *  InterestRate; //mais uma vez somente dando certo com o "protected da varável balance"
+    }
+}
--- Exercicios Java/src/Object_Orientation/Inheritance/Test/Program.java
+++ Exercicios Java/src/Object_Orientation/Inheritance/Test/Program.java
@@ -1,11 +1,35 @@
 package Object_Orientation.Inheritance.Test;
 
+import Object_Orientation.Inheritance.Entities.Account;
 import Object_Orientation.Inheritance.Entities.BusinessAccount;
+import Object_Orientation.Inheritance.Entities.SavingsAccount;
 
 public class Program {
     static void main() {
-        BusinessAccount account = new BusinessAccount();
-        account.getBalance();
-        account.getHolder();
+        BusinessAccount example = new BusinessAccount();
+        example.getBalance();
+        example.getHolder();
+
+        Account account = new Account(10, 0.0, "Joao" );
+        BusinessAccount busAcc = new BusinessAccount(5, 500.0, "Maria", 500.0);
+
+        //UPCASTING
+
+        Account acc = busAcc;
+        Account acc1 = new BusinessAccount(9, 1000.0, "Alex", 2000.0);
+        Account acc2 = new SavingsAccount(100, 3000.0, "Ana", 0.01);
+
+        //DOWNCASTING
+
+        BusinessAccount acc3 = (BusinessAccount) acc1;
+
+        //BusinessAccount acc4 = (BusinessAccount) acc2; acc2 não é business account.
+
+        if (acc2 instanceof BusinessAccount) {
+            BusinessAccount busAcc5 = (BusinessAccount) acc2;
+            busAcc5.loan(200.0);
+            System.out.println("Done Loan!");
+
+        }
     }
 }
